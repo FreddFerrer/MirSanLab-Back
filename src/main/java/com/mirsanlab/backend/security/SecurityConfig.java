f2ae8818163f904c9986052efed47cc31e5696e2
Freddy <ferreirafreddy39@gmail.com>
2025-06-05 23:38:20 -0300

    ready

--- src/main/java/com/mirsanlab/backend/security/SecurityConfig.java
+++ src/main/java/com/mirsanlab/backend/security/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig {
                 .cors(cors -> cors
                         .configurationSource(request -> {
                             var config = new org.springframework.web.cors.CorsConfiguration();
-                            config.setAllowedOrigins(List.of("http://localhost:4200", "http://srv855052.hstgr.cloud:80", "http://srv855052.hstgr.cloud"));
+                            config.setAllowedOrigins(List.of("http://localhost:4200", "http://srv855052.hstgr.cloud:80",
+                                    "http://srv855052.hstgr.cloud", "http://31.97.29.189"));
                             config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                             config.setAllowedHeaders(List.of("*"));
                             config.setAllowCredentials(true);
